1d13ade9240789c0be3d47eb4837b379a5b31c57
wavyhairdude <angel0615@AngelsHPEnvy.localdomain>
2022-04-13 22:33:56 -0400

    new working in java

--- javasrc/Sensor.java
+++ javasrc/Sensor.java
@@ -1,14 +1,26 @@
 import java.util.concurrent.atomic.AtomicMarkableReference;
 
+
 class Queue
 {
-    Node start, end;
+    Node start;
     Queue()
     {
+        // initialize and set to minimum value
         this.start = new Node(-500);
+        Node n = start;
+        // create placeholder nodes with min values
+        // these will always get replaced
+		for (int i = 0; i < 5; i++)
+		{
+			n.next = new AtomicMarkableReference<Node>(new Node(-500), false);
+			n = n.next.getReference();
+		}
+
+		//this.printQ();
     }
 
-    /// Done. Checks to find the spot in which we can continue getting the values
+    /// Checks to find the spot in which we can continue getting the values
 	/// for adding and removing, physically deletes nodes
 	private Node[] getLoc(int x)
 	{
@@ -43,7 +55,7 @@ class Queue
 					following = curr.next.get(marked);
 				}
 
-				if (curr.val > x)
+				if (curr.val >= x)
 				{
 					t[0] = curr;
 					t[1] = prev;
@@ -56,55 +68,150 @@ class Queue
 		}
 	}
 
-    // TODO
+    /// add node at correct position, if posssible, otherwise ignore
+    /// if adding a node, remove the next lowest
     public boolean addNode(int temp)
     {
-        // check if the node is less than the queue
-        Node[] xy = this.getLoc(temp);
-        Node curr = xy[0], prev = xy[1];
+        Node n = new Node(temp);
 
+
+        while (true)
+        {
+            Node[] xy = getLoc(temp);
+            Node curr = xy[0], prev = xy[1];
+
+            if (curr != null && (curr.val == temp ))
+                return false;
+
+            n.next = new AtomicMarkableReference<Node>(curr, false);
+            if (prev.next.compareAndSet(curr, n, false, false))
+            {
+                removeFirst();
+                return true;
+            }
+        }
+    }
+
+    /// simply print the queue in the order it is placed
+    public void printQ(boolean reverse)
+    {
+        Node c = this.start.next.getReference();
+        while (c != null)
+        {
+            if (reverse)
+                System.out.print(c.val * -1 + " ");
+            else
+                System.out.print(c.val + " ");
+            c = c.next.getReference();
+        }
+
+        System.out.println();
+    }
+
+    // order N operation to get the last value
+    public int getTail(boolean reversed)
+    {
+        int c = 500;
         
+        Node[] xy = getLoc(c);
+        int z = xy[1].val;
+        return (reversed? -1: 1) * z;
+    }
 
-        return true;
+    /// remove the first element in the list
+    public void removeFirst()
+    {
+        this.start = this.start.next.getReference();
     }
 }
 
 
 public class Sensor extends Thread
 {
-    static Queue one;
-    static int numSensors = 8;
-    static int INTERVAL = 100;
+    static Queue inc, dec;
+    static int numSensors = 1;
+    static int INTERVAL = 10;
+    static boolean DEBUG = false;
+    static boolean PRINT_REPORTS = false;
+    // interval in ms
 
     public void run()
     {   
-        for (int i = 0; i < 60; i++)
+        if (DEBUG)
+            System.out.println("thread started + " + this.getId());
+
+        // once per minute, record time, for total of 10 minutes
+        for (int i = 0; i < 10; i++)
         {
             // temp ranges from -100 -> 70F
             int temp = (int)(Math.random() * 170) + -100;
 
-            //TODO add node
-            one.addNode(temp);
+            // add node (removing will be built in), negative for the decreasing order
+            inc.addNode(temp);
+            dec.addNode(-1 * temp);
 
+            // wait one user designated minute, default is 100ms
             try { Thread.sleep(INTERVAL);}
             catch (Exception e) { System.out.println("Error:" + e); };
         }
 
+        if (DEBUG)
+                System.out.println("Thread finished " + this.getId() );
+
     }
 
     public static void main(String[] args) {
-        one = new Queue();
+        inc = new Queue();
+        dec = new Queue();
+        
         Sensor[] pool = new Sensor[numSensors];
-        for (int i = 0; i < numSensors; i++)
+        int maxDiff = 0, maxInterval = 0;
+
+        System.out.println("Beginning the simulation!");
+        for (int j = 0; j <= 6; j++)
         {
-            pool[i] = new Sensor();
-            pool[i].start();
+            // start the threads monitoring for 10 minutes each
+            for (int i = 0; i < numSensors; i++)
+            {
+                pool[i] = new Sensor();
+                pool[i].start();
+            }
+
+            // get the largest value, dont modify.
+            for (int i = 0; i < numSensors; i++)
+                try {pool[i].join();} catch (Exception e) {;}
+
+            // check if this is the instance where the diff is the greatest
+            int diff = Math.abs(inc.getTail(false) - dec.getTail(true));
+            if (diff > maxDiff)
+            {
+                maxDiff = diff;
+                maxInterval = j;
+            }
+            
+            if (PRINT_REPORTS)
+            { 
+                System.out.println("Report for "+(10 * j)+" mins:");
+                System.out.print("Maximums: ");
+                inc.printQ(false);
+                System.out.print("Minimums: ");
+                dec.printQ(true);
+                System.out.println();
+            }
         }
 
-        for (int i = 0; i < numSensors; i++)
-            try {pool[i].join();} catch (Exception e) {;}
 
-        System.out.println("Everything has been sensed");
+        System.out.println("10 min interval with greatest diff = " + (maxInterval * 10) + "mins in");
+
+        System.out.println("Final Report:");
+        System.out.print("Maximums: ");
+        inc.printQ(false);
+        System.out.print("Minimums: ");
+        dec.printQ(true);
+        System.out.println();
+        
+        System.out.println("Namaste, Everything has been sensed");
+
 
     }
 }
